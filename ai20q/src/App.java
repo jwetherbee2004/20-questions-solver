1ab1614007d8a7899c039095ad51b328bf5c49f4
jwetherbee2004 <jared.wetherbee04@gmail.com>
2025-11-11 11:28:00 -0500

    made the path in App.java relative

--- ai20q/src/App.java
+++ ai20q/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        KnowledgeBase kb = new KnowledgeBase("C:\\Users\\jarja\\Desktop\\School\\B351\\20 Questions Solver\\20-questions-solver\\animals.json");
+        KnowledgeBase kb = new KnowledgeBase("animals.json");
         System.out.println("Animals loaded: " + kb.getAnimals().size());
         for (String animal : kb.getAnimals()) {
             System.out.println("Animal: " + animal + ", Attributes: " + kb.getAttributes(animal).size());
