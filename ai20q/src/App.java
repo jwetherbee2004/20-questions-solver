47d793f724db5c413308b5df414cf32b0a71d5a0
jwetherbee2004 <jared.wetherbee04@gmail.com>
2025-11-12 14:54:39 -0500

    made a bare bones implementation for the 20 questions solver

--- ai20q/src/App.java
+++ ai20q/src/App.java
@@ -1,9 +1,55 @@
+import java.util.Scanner;
+
 public class App {
+    private static Boolean solved = false;
     public static void main(String[] args) throws Exception {
         KnowledgeBase kb = new KnowledgeBase("animals.json");
-        System.out.println("Animals loaded: " + kb.getAnimals().size());
-        for (String animal : kb.getAnimals()) {
-            System.out.println("Animal: " + animal + ", Attributes: " + kb.getAttributes(animal).size());
+        Solver solver = new Solver(kb.getData());
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Think of an animal, and I will try to guess it.");
+
+        while (!solved) {
+            String step = solver.makeGuess();
+            if (step == null) {
+                System.out.println("I don't have any more questions or guesses. I give up.");
+                break;
+            }
+
+            if (step.startsWith("ATTR:")) {
+                String attr = step.substring("ATTR:".length());
+                System.out.println(attr + "? (y/n)");
+                String ans = sc.nextLine().trim().toLowerCase();
+                if (ans.startsWith("y")) {
+                    solver.applyAnswer(attr, true);
+                } else if (ans.startsWith("n")) {
+                    solver.applyAnswer(attr, false);
+                } else {
+                    System.out.println("Please answer 'y' or 'n'.");
+                }
+                // loop continues: solver.makeGuess will choose next attribute or concrete guess
+            } else if (step.startsWith("ANIMAL:")) {
+                String animal = step.substring("ANIMAL:".length());
+                System.out.println("Is it a " + animal + "? (y/n)");
+                String ans = sc.nextLine().trim().toLowerCase();
+                if (ans.startsWith("y")) {
+                    System.out.println("Yay! I guessed it right.");
+                    solved = true;
+                } else if (ans.startsWith("n")) {
+                    // if there are more concrete guesses, loop will continue and solver.makeGuess() will return the next
+                    if (!solver.hasMoreConcreteGuesses()) {
+                        System.out.println("I couldn't guess your animal from my knowledge base.");
+                        break;
+                    }
+                } else {
+                    System.out.println("Please answer 'y' or 'n'.");
+                }
+            } else {
+                System.out.println("Unexpected solver response: " + step);
+                break;
+            }
         }
+
+        sc.close();
     }
 }
--- ai20q/src/KnowledgeBase.java
+++ ai20q/src/KnowledgeBase.java
@@ -1,16 +1,21 @@
-
-
 import com.google.gson.*;
 import java.io.*;
 import java.util.*;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class KnowledgeBase {
     private Map<String, Map<String, Boolean>> data;
 
-    public KnowledgeBase(String filePath) throws IOException {
+    public KnowledgeBase(String resourceName) throws IOException {
         Gson gson = new Gson();
-        Reader reader = new FileReader(filePath);
-        this.data = gson.fromJson(reader, Map.class);
+        InputStream is = getClass().getClassLoader().getResourceAsStream(resourceName);
+        if (is == null) {
+            throw new FileNotFoundException(resourceName + " not found on classpath");
+        }
+        Reader reader = new InputStreamReader(is, StandardCharsets.UTF_8);
+        this.data = gson.fromJson(reader, new com.google.gson.reflect.TypeToken<Map<String, Map<String, Boolean>>>(){}.getType());
         reader.close();
     }
 
@@ -20,5 +25,9 @@ public class KnowledgeBase {
 
     public Map<String, Boolean> getAttributes(String animal) {
         return data.get(animal);
+    }
+
+    public Map<String, Map<String, Boolean>> getData() {
+        return data;
     }
 }
